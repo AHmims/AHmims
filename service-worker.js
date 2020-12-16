bbcb9a3c7c2a83de43116a63e06e632a38091fb7
AHmims <ali.hmims99@gmail.com>
2020-12-16 21:24:44 +0100

    pwa ready

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-console.log("Hello from service-worker.js");
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
+);
+//
+workbox.routing.registerRoute(function ({ request }) {
+  request.destination == "image";
+}, new workbox.strategies.CacheFirst());
